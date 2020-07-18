547a79727294142261c3f91a8ab178b80b09d242
Carsten <cd@ordrestyring.dk>
2020-07-18 11:43:18 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
-chrome.storage.local.get('enabled', data => {
-    if (data.enabled) {
-        //it is enabled, do accordingly
+if (/^https:\/\/(.*)?\.atlassian\.net\/secure\/RapidBoard.jspa\?rapidView/gmi.test(window.location.href)) {
+    const PAGE_UP = 33;
+    const PAGE_DOWN = 34;
+    const ARROW_UP = 38;
+    const ARROW_DOWN = 40;
 
-        if (/^https:\/\/(.*)?\.atlassian\.net\/secure\/RapidBoard.jspa\?rapidView/gmi.test(window.location.href)) {
-            const PAGE_UP = 33;
-            const PAGE_DOWN = 34;
-            const ARROW_UP = 38;
-            const ARROW_DOWN = 40;
+    $(document).ready(() => {
+        $('.ghx-swimlane').not('.ghx-closed').each((_, elem) => $(elem).addClass('ghx-closed'));
+        $('.ghx-swimlane').last().addClass('ghx-last')
+        $('.ghx-swimlane.ghx-first').removeClass('ghx-closed');
 
-            $(document).ready(() => {
-                $('.ghx-swimlane').not('.ghx-closed').each((_, elem) => $(elem).addClass('ghx-closed'));
-                $('.ghx-swimlane').last().addClass('ghx-last')
-                $('.ghx-swimlane.ghx-first').removeClass('ghx-closed');
+        $('body').keydown(({ keyCode, preventDefault }) => {
+            preventDefault();
+            const current = $('.ghx-swimlane:not(.ghx-closed)');
+            const nextKeys = (keyCode === ARROW_DOWN || keyCode === PAGE_DOWN);
+            const previousKeys = (keyCode === ARROW_UP || keyCode === PAGE_UP);
 
-                $('body').keydown(({ keyCode, preventDefault }) => {
-                    preventDefault();
-                    const current = $('.ghx-swimlane:not(.ghx-closed)');
-                    const nextKeys = (keyCode === ARROW_DOWN || keyCode === PAGE_DOWN);
-                    const previousKeys = (keyCode === ARROW_UP || keyCode === PAGE_UP);
+            if (nextKeys && !$(current).hasClass('ghx-last')) {
+                $(current).addClass('ghx-closed');
+                $(current).next().removeClass('ghx-closed')
+            }
 
-                    if (nextKeys && !$(current).hasClass('ghx-last')) {
-                        $(current).addClass('ghx-closed');
-                        $(current).next().removeClass('ghx-closed')
-                    }
-
-                    if (previousKeys && !$(current).hasClass('ghx-first')) {
-                        $(current).addClass('ghx-closed');
-                        return $(current).prev().removeClass('ghx-closed')
-                    }
-                });
-            });
-        }
-    }
-});
+            if (previousKeys && !$(current).hasClass('ghx-first')) {
+                $(current).addClass('ghx-closed');
+                return $(current).prev().removeClass('ghx-closed')
+            }
+        });
+    });
+}
