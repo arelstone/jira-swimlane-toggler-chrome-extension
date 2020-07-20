cb9c304365f4ebaa66c50c88223b6ba0cf68dda9
Carsten <cd@ordrestyring.dk>
2020-07-20 12:43:39 +0200

    removed text to speech

--- index.js
+++ index.js
@@ -9,58 +9,24 @@ if (/^https:\/\/(.*)?\.atlassian\.net\/secure\/RapidBoard.jspa\?rapidView/gmi.te
         $('.ghx-swimlane').last().addClass('ghx-last')
         $('.ghx-swimlane.ghx-first').removeClass('ghx-closed');
 
-        speak(name());
-
         $('body').keydown(({ keyCode, preventDefault }) => {
             preventDefault();
+
             const current = $('.ghx-swimlane:not(.ghx-closed)');
             const nextKeys = (keyCode === ARROW_DOWN || keyCode === PAGE_DOWN);
             const previousKeys = (keyCode === ARROW_UP || keyCode === PAGE_UP);
+            const isFirst = $(current).hasClass('ghx-first');
+            const isLast = $(current).hasClass('ghx-last');
 
-            if (nextKeys && !$(current).hasClass('ghx-last')) {
+            if (nextKeys && !isLast) {
                 $(current).addClass('ghx-closed');
                 $(current).next().removeClass('ghx-closed');
-                speak(name());
             }
 
-            if (previousKeys && !$(current).hasClass('ghx-first')) {
+            if (previousKeys && !isFirst) {
                 $(current).addClass('ghx-closed');
                 $(current).prev().removeClass('ghx-closed');
-                speak(name());
             }
         });
     });
-}
-
-const speak = (name) => {
-    if ('speechSynthesis' in window) {
-
-        var synthesis = window.speechSynthesis;
-
-        // Get the first `en` language voice in the list
-        var voice = synthesis.getVoices().filter(function (voice) {
-            return voice.lang === 'en';
-        })[0];
-
-        // Create an utterance object
-        var utterance = new SpeechSynthesisUtterance(name);
-
-        // Set utterance properties
-        utterance.voice = voice;
-        utterance.pitch = 1.0;
-        utterance.rate = 1.0;
-        utterance.volume = 1;
-
-        // Speak the utterance
-        synthesis.speak(utterance);
-
-    } else {
-        console.log('Text-to-speech not supported.');
-    }
-}
-
-const name = () => {
-    return document.querySelector('.ghx-swimlane:not(.ghx-closed)')
-        .querySelector('.ghx-swimlane-header')
-        .querySelector('.ghx-heading').getAttribute('data-tooltip')
 }
