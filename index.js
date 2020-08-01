7c60c8a722c3a95c7d00c0bb1a50f3ab91587dd7
Loris Bettazza <loris.bettazza@gmail.com>
2020-08-01 22:41:28 +0200

    Fix incorrect code translation from jQuery
    More checks are needed before changing classes (in case an element is null), unfortunately it makes the code more verbose

--- index.js
+++ index.js
@@ -13,30 +13,39 @@ if (/^https:\/\/(.*)?\.atlassian\.net\/secure\/RapidBoard.jspa\?rapidView/gmi.te
     const ARROW_DOWN = 40;
 
     onDocumentReady(() => {
-        const swimlanes = Array.from(document.querySelectorAll('.ghx-swimlane'));
+        const swimlanes = document.querySelectorAll('.ghx-swimlane');
 
         swimlanes.forEach(elem => elem.classList.add('ghx-closed'));
-        swimlanes.slice(-1)[0].classList.add('ghx-last');
-        swimlanes.querySelector('.ghx-first').classList.remove('ghx-closed');
+        const last = swimlanes[swimlanes.length - 1];
+        if (last) last.classList.add('ghx-last');
+        swimlanes.forEach(elem => {
+            if (elem.classList.contains('ghx-first')) {
+                elem.classList.remove('ghx-closed');
+            }
+        });
 
         document.querySelectorAll('body, #content, #ghx-pool').forEach(elem =>
-            elem.addEventListener('keydown', ({ keyCode, preventDefault }) => {
-                preventDefault();
-
+            elem.addEventListener('keydown', e => {
                 const current = document.querySelector('.ghx-swimlane:not(.ghx-closed)');
-                const nextKeys = keyCode === ARROW_DOWN || keyCode === PAGE_DOWN;
-                const previousKeys = keyCode === ARROW_UP || keyCode === PAGE_UP;
+                if (!current) return;
+
+                const nextKeys = e.keyCode === ARROW_DOWN || e.keyCode === PAGE_DOWN;
+                const previousKeys = e.keyCode === ARROW_UP || e.keyCode === PAGE_UP;
+                if (nextKeys || previousKeys) e.preventDefault();
+
                 const isFirst = current.classList.contains('ghx-first');
                 const isLast = current.classList.contains('ghx-last');
 
                 if (nextKeys && !isLast) {
                     current.classList.add('ghx-closed');
-                    current.nextElementSibling.classList.remove('ghx-closed');
+                    const next = current.nextElementSibling;
+                    if (next) next.classList.remove('ghx-closed');
                 }
 
                 if (previousKeys && !isFirst) {
                     current.classList.add('ghx-closed');
-                    current.previousElementSibling.classList.remove('ghx-closed');
+                    const prev = current.previousElementSibling;
+                    if (prev) prev.classList.remove('ghx-closed');
                 }
             })
         );
